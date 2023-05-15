b771a5784a6deb42c1ad2526dfa0dad35b2d8343
mumbaiindians1234 <zemlebarda@gufum.com>
2023-05-15 14:50:05 +0530

    main

---
+++ script.js
@@ -0,0 +1,49 @@
+
+/* blur refresh */
+
+function blurAndRefresh() {
+    // Add the blur class to the body element
+    document.body.classList.add('blur');
+    
+    // Wait for 1 seconds
+    setTimeout(function() {
+      // Refresh the page
+      location.reload();
+    }, 2000);
+  }
+
+
+  document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+
+
+
+
+
+
+
+/* drag and drop */
+
+
+/*      */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
